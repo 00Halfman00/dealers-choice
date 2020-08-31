fe3b972a4aa736fcf71a5d3344f8717e8279eb93
Oscar Sanchez <oscarsanchez@Oscars-MacBook-Pro.local>
2020-08-31 16:28:28 -0500

    got webpack running and routes displaying data

--- server.js
+++ server.js
@@ -2,15 +2,36 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
-const db = require('./db');
+const { Album, Artist, Song, conn} = require('./db/index');
 
-
+//logging middleware
 app.use(morgan('dev'));
-app.use(require('body-parser').json());
+
+//body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.get('/albums', async (req, res, next) => {
+    try {
+        res.send(await Album.findAll({include:[Artist]}))
+    } catch(err){
+        next(err);
+    };
+});
+
+app.get('/albums/:albumId', async (req, res, next) => {
+    try {
+        res.send(await Album.findByPk(req.params.albumId, { include: [Artist, Song]} ))
+    } catch(err) {
+        next(err);
+    };
 });
 
 app.use((err, req, res, next) => {

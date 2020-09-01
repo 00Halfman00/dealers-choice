d04b0983a1ce58eff1fc5a0da18fa0fa337021a3
Oscar Sanchez <oscarsanchez@Oscars-MacBook-Pro.local>
2020-09-01 10:49:51 -0500

    okay, next deploy on heroku

--- src/index.js
+++ src/index.js
@@ -9,55 +9,106 @@ class Main extends React.Component {
       albums: [],
       selectedAlbum: {},
     };
+    this.getAlbum = this.getAlbum.bind(this);
+  }
+
+  async getAlbum(albumId, select = true) {
+    if (select) {
+      const res = await axios.get(`/albums/${albumId}`);
+      console.log('in getAlbum ', res.data);
+      this.setState({ selectedAlbum: res.data });
+    } else {
+      this.setState({ selectedAlbum: {} });
+    }
   }
 
   async componentDidMount() {
     const res = await axios.get('/albums/');
-    this.setState({albums: res.data});
+    this.setState({ albums: res.data });
   }
 
   render() {
-      //console.log(this.state.albums)
+    const renderObj = this.state.selectedAlbum.name ? (
+      <SingleAlbum album={this.state.selectedAlbum} />
+    ) : (
+      <Display albums={this.state.albums} getAlbum={this.getAlbum} />
+    );
+
     return (
-    <div>
-    <Sidebar />
-    <Display albums={this.state.albums}/>
-    </div>
-    )
+      <div className="container">
+        <Sidebar getAlbum={this.getAlbum} />
+        {renderObj}
+      </div>
+    );
   }
 }
 
-const Sidebar = () => {
+const Sidebar = ({ getAlbum }) => {
+  console.log('in Sidebar ', getAlbum);
   return (
     <div id="albums" className="row wrap">
-      <a>Albums</a>
+      <h3>
+        <a onClick={() => getAlbum(0, false)}>Albums</a>
+      </h3>
     </div>
   );
 };
 
-const Display = ({ albums }) => {
-    console.log('in Display ', albums)
-    return (
-        <div id="albums" className="row wrap">
-            {albums.map( album => 
-                <AlbumDiv album={album} key={album.id} />
-                )}
-        </div>
-    )
-}
+const Display = ({ albums, getAlbum }) => {
+  console.log('in Display ', getAlbum);
+  return (
+    <div id="albums" className="row wrap">
+      {albums.map((album) => (
+        <AlbumDiv album={album} key={album.id} getAlbum={getAlbum} />
+      ))}
+    </div>
+  );
+};
 
-const AlbumDiv = ( { album }) => {
-    console.log('in AlbumDiv ', album)
-    return (
-        <div className="album">
-            <a>
-                <img src={album.artworkUrl} />
-                <p>{album.name}</p>
-                <small>{album.artist.name}</small>
-            </a>
-        </div>
-    )
-}
+const AlbumDiv = ({ album, getAlbum }) => {
+  console.log('in AlbumDiv ', getAlbum);
+  return (
+    <div className="album">
+      <a onClick={() => getAlbum(album.id)}>
+        <img src={album.artworkUrl} />
+        <p>{album.name}</p>
+        <small>{album.artist.name}</small>
+      </a>
+    </div>
+  );
+};
 
+const SongInfo = ({ song, artist }) => {
+  return (
+    <tr>
+      <td>1</td>
+      <td>{song.name}</td>
+      <td>{artist}</td>
+      <td>{song.genre}</td>
+    </tr>
+  );
+};
+
+const SingleAlbum = ({ album }) => {
+  return (
+    <div id="single-album" className="column">
+      <AlbumDiv album={album} />
+      <table id="songs">
+        <tbody>
+          <tr className="gray">
+            <td />
+            <td>#</td>
+            <td>Name</td>
+            <td>Artist</td>
+            <td>Genre</td>
+          </tr>
+          {album.songs.map((song) => (
+            <SongInfo song={song} artist={album.artist.name} key={song.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 ReactDOM.render(<Main />, document.getElementById('root'));
